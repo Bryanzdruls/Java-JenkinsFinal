100bdb7f57694f8b29445b94272b111397a46429
Bryanzdruls <eltiobryanz@gmail.com>
2024-05-25 11:53:41 -0500

    primer commit

---
+++ src/main/java/com/example/restapi/controller/RouterRest.java
@@ -0,0 +1,28 @@
+package com.example.restapi.controller;
+
+import com.example.restapi.repository.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class RouterRest {
+
+    @GetMapping("/get-users")
+    public List<User> getUsers(){
+        List<User> users = new ArrayList<>();
+        users.add(new User("1023","Brian","eltiobryanz@gmail.com"));
+        users.add(new User("1021","Juan","JJGG@gmail.com"));
+        users.add(new User("1023","Lucho","lucho:)@gmail.com"));
+        return users;
+    }
+
+    @PostMapping("/add-user")
+    public User addUser(@RequestBody  User user){
+        return new User(user.getId(), user.getName(), user.getEmail());
+    }
+}
